b7c6ed990eda48f43f5fad5acc25ca970cc95335
JP <jay.patel.py@gmail.com>
2023-12-17 10:19:42 -0600

    Add styling and images to LoginScreen component

--- app/screen/auth/LoginScreen.jsx
+++ app/screen/auth/LoginScreen.jsx
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function LoginScreen() {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 50,
+    }}>
+      <Image 
+        source={require("../../../assets/images/logo.png")}
+        style={styles.logoImage}
+      />
+      <Image 
+        source={require("../../../assets/images/logo-bg.png")}
+        style={styles.bgImage}
+      />
+      <View style={{padding: 20}}>
+        <Text style={styles.heading}>Your Ultimate EV Charging Station Finder App</Text>
+        <Text style={styles.desc}>Find EV Charger near you, Plan trip and so much more in just one click</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  logoImage: {
+    width: 200,
+    height: 40,
+    objectFit: "contain",
+  },
+  bgImage: {
+    width: "100%",
+    height: 240,
+    marginTop: 20,
+    objectFit: "cover",
+  },
+  heading: {
+    fintSize: 25,
+    fontFamily: "Outfit-bold",
+    textAlign: "center",
+    marginTop: 20,
+  },
+  desc: {
+    fontSize: 17,
+    fontFamily: "Outfit",
+    marginTop: 15,
+    textAlign: "center",
+  }
+})
