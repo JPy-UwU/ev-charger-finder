6b1a2173b7c068afb4fc26cc3c0d4f8c7c17e7c0
JP <jay.patel.py@gmail.com>
2023-12-27 11:29:30 -0600

    Add header component to HomeScreen

--- app/screen/home/HomeScreen.jsx
+++ app/screen/home/HomeScreen.jsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 import AppMapView from './AppMapView';
+import Header from './Header';
 
 export default function HomeScreen() {
   return (
     <View>
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
       <AppMapView />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    padding: 10,
+    paddingHorizontal: 20,
+    zIndex: 10,
+  }
+})
